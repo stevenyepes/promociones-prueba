a293eebb6dd8f23823ddcda87d1af5edd0a11036
Jhonatan Yepes <jstevenyepes@ggmail.com>
2020-07-30 00:11:59 -0500

    Se agrega consulta por id o por brand y descripcion

--- src/products/product.schema.ts
+++ src/products/product.schema.ts
@@ -2,29 +2,37 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 import { ApiProperty } from '@nestjs/swagger';
 
+export interface IProduct {
+    id: number;
+    brand: string;
+    description: string;
+    image: string;
+    price: number;
+
+}
 
 @Schema()
-export class Product extends Document {
+export class Product extends Document implements IProduct{
     
     @Prop()
     @ApiProperty({ example: 1, description: 'Identificador único del producto' })
-    id: number;
+    id: IProduct['id'];
 
     @ApiProperty({ example: 'weñxoab', description: 'Marca del producto' })
     @Prop()
-    brand: string;
+    brand: IProduct['brand'];
     
     @ApiProperty({ example: 'pnyn rlxbewnk', description: 'Descripción del producto' })
     @Prop()
-    description: string;
+    description: IProduct['description'];
 
     @ApiProperty({ example: 'trcwl iagxxh', description: 'URI de la imágen' })
     @Prop()
-    image: string
+    image: IProduct['image'];
 
     @ApiProperty({ example: 200, description: 'Precio del producto' })
     @Prop()
-    price: number
+    price: IProduct['price'];
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
-import { Product} from "./product.schema";
+import { Product } from "./product.schema";
 import { getModelToken } from '@nestjs/mongoose';
 
 describe('ProductsService', () => {
@@ -8,7 +8,11 @@ describe('ProductsService', () => {
 
   class ProductModel {
     constructor(private data) {}
-    static find = jest.fn().mockResolvedValue([]);
+    static find = jest.fn().mockResolvedValue([{
+      id: 5,
+      brand: 'asdad',
+      description: 'dd'
+  }]);
     
   }
 
@@ -30,4 +34,13 @@ describe('ProductsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should search a product by Id', async () => {
+
+    const product = await service.findById(5);    
+    expect(product).toBeDefined();
+    expect(product[0].id).toBe(5);
+    expect(product[0].brand).toBe('asdad');
+  });
+
 });
