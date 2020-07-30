7e1c002bba7bbff1291041f9e5cf067ad564b643
Jhonatan Yepes <jstevenyepes@ggmail.com>
2020-07-29 21:32:46 -0500

    Se agegan test para CI

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
+jest.mock('./products.service');
 
 describe('Products Controller', () => {
   let controller: ProductsController;
 
+
+  
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
+      providers: [ProductsService]
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
